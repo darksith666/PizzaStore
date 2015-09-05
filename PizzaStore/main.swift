5246a8bf09353df870e2368e51e556e4fe38e141
takushi <takushi@mfmf.me>
2015-09-05 10:54:21 +0900

    Initial Commit

---
+++ PizzaStore/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  PizzaStore
+//
+//  Created by Homma Takushi on 2015/09/05.
+//  Copyright (c) 2015年 mfmf.me. All rights reserved.
+//
+
+import Foundation
+
+println("Hello, World!")
+
